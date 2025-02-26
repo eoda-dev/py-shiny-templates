1c375f3275f6211bb4df0cffa43ab3bdc51d5464
Stefan Kuethe <stefan.kuethe@eoda.de>
2025-02-26 10:13:56 +0100

    Refactor

--- starlette-oauth/app.py
+++ starlette-oauth/app.py
@@ -1,33 +1,33 @@
 import json
-from starlette.config import Config
+
+from authlib.integrations.starlette_client import OAuth
 from starlette.applications import Starlette
+from starlette.config import Config
 from starlette.middleware.sessions import SessionMiddleware
 from starlette.responses import HTMLResponse, RedirectResponse
-from authlib.integrations.starlette_client import OAuth
-from app_shiny2 import app as shiny_app
 from starlette.routing import Mount
+
+from app_shiny import app as shiny_app
 
 app = Starlette(debug=True)
 app.add_middleware(SessionMiddleware, secret_key="!secret")
 
-config = Config('.env')
+config = Config(".env")
 oauth = OAuth(config)
 
 CONF_URL = "https://gitlab.com/.well-known/openid-configuration"
 oauth.register(
-    name='gitlab',
+    name="gitlab",
     server_metadata_url=CONF_URL,
-    client_id = config.file_values["GITLAB_CLIENT_ID"],
-    client_secret = config.file_values["GITLAB_CLIENT_SECRET"],
-    client_kwargs={
-        'scope': 'openid email profile'
-    }
+    client_id=config.file_values["GITLAB_CLIENT_ID"],
+    client_secret=config.file_values["GITLAB_CLIENT_SECRET"],
+    client_kwargs={"scope": "openid email profile"},
 )
 
 
-@app.route('/')
+@app.route("/")
 async def homepage(request):
-    user = request.session.get('user')
+    user = request.session.get("user")
     print("starlette session", request.session)
     if user:
         data = json.dumps(user)
@@ -41,29 +41,31 @@ async def homepage(request):
     return HTMLResponse('<a href="/login">login</a>')
 
 
-@app.route('/login')
+@app.route("/login")
 async def login(request):
-    redirect_uri = request.url_for('auth')
+    redirect_uri = request.url_for("auth")
     print(redirect_uri)
     return await oauth.gitlab.authorize_redirect(request, redirect_uri)
 
 
-@app.route('/auth')
+@app.route("/auth")
 async def auth(request):
     token = await oauth.gitlab.authorize_access_token(request)
-    user = token.get('userinfo')
+    user = token.get("userinfo")
     if user:
-        request.session['user'] = user
-    return RedirectResponse(url='/')
+        request.session["user"] = user
+    return RedirectResponse(url="/")
 
 
-@app.route('/logout')
+@app.route("/logout")
 async def logout(request):
-    request.session.pop('user', None)
-    return RedirectResponse(url='/')
+    request.session.pop("user", None)
+    return RedirectResponse(url="/")
+
 
 app.routes.append(Mount("/shiny", app=shiny_app))
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host='127.0.0.1', port=8000)
+
+    uvicorn.run(app, host="127.0.0.1", port=8000)
--- starlette-oauth/app_shiny.py
+++ starlette-oauth/app_shiny.py
@@ -1,26 +1,28 @@
-from shiny import App, render, ui
 from htmltools import a
+from shiny import App, render, ui
 
 ui = ui.page_fluid(
     ui.panel_title("Hello from your Secure Shiny App!"),
     ui.output_text_verbatim("txt"),
-    ui.output_ui("links")
+    ui.output_ui("links"),
 )
+
 
 def server(input, output, session):
     @render.ui
     def links():
         if session.http_conn.session.get("user") is None:
             return a("login", href="/login")
-        
+
         return a("logout", href="/logout")
-        
+
     @render.text
     def txt():
         user = session.http_conn.session.get("user")
         if user is None:
             return "Please login to view this page!"
-        
+
         return f"Hi {user['nickname']}, your email is {user['email']}!"
+
 
 app = App(ui, server)
